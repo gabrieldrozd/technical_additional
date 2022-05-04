0f5bdacafc562c76b0cd3a17d2bc129b1b002627
Gabriel <drozd.gj@gmail.com>
2022-05-04 16:19:01 +0200

    final final version

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,11 +1,6 @@
-﻿using ContractorMng.Service;
-using ContractorMng.Service.Contract;
+﻿using ContractorMng.Service.Contract;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.ServiceModel;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Client
 {
@@ -14,23 +9,19 @@ namespace Client
         static void Main(string[] args)
         {
             IContractorService _contractorServiceClient = new ChannelFactory<IContractorService>("ContractorService").CreateChannel();
-            Console.WriteLine("1. Wypisanie wszystkich contractorĂłw");
-            var caseOpt = Console.ReadLine();
-            switch (caseOpt)
+            Console.WriteLine("Naciśnij dowolny klawisz, aby wypisać wszystkich Contractors...");
+            Console.ReadLine();
+            
+            var contractors = _contractorServiceClient.GetAllContractors();
+            foreach (var contractor in contractors)
             {
-                case "1":
-                    var contractors = _contractorServiceClient.GetAllContractors();
-                    foreach (var item in contractors)
-                    {
-                        Console.WriteLine("Id: " + item.ContractorId);
-                        Console.WriteLine("Email: " + item.Email);
-                        Console.WriteLine("Name: " + item.Name);
-                        Console.WriteLine("Nip: " + item.Nip);
-                        Console.WriteLine("PhoneNo: " + item.PhoneNo);
-                        Console.WriteLine();
-                    }
-                    break;
+                Console.WriteLine("Name: " + contractor.Name);
+                Console.WriteLine("Email: " + contractor.Email);
+                Console.WriteLine("PhoneNo: " + contractor.PhoneNo);
+                Console.WriteLine("Nip: " + contractor.Nip);
+                Console.WriteLine();
             }
+
             Console.ReadKey();
         }
     }
